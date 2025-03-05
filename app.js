305bc5558874656d7b396d3855f84895b9639f65
Saurabh Tomar <saurabhtomar@Saurabhs-MacBook-Air.local>
2025-03-05 11:18:32 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+
+const element = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "I am h1"), React.createElement("h2", {}, "I am h2")]));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(element);
