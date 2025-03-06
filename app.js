5c43b40f4a824a5dcb4b7a83ac61f8cf32d49248
Saurabh Tomar <saurabhtomar@Saurabhs-MacBook-Air.local>
2025-03-06 09:36:46 +0530

    module 3 code

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const element = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "I am h1"), React.createElement("h2", {}, "I am h2")]));
+// const element = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "I am h1"), React.createElement("h2", {}, "I am h2")]));
+const JsxElement = () => (
+    <div id="parent">
+        <div id="child">
+            <h1>I am h1</h1>
+            <h2>I am h2</h2>
+        </div>
+    </div>
+);
+const JsxElementComponent = () => (
+    <div id="parent">
+        <div id="child">
+            <h1>I am h1</h1>
+            <h2>I am h2</h2>
+        </div>
+    </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(element);
+root.render(<JsxElementComponent />);
