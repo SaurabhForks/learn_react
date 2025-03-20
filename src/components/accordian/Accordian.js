93e45f008885530c568ae20f95de360571de6997
Saurabh Tomar <saurabhtomar@Saurabhs-MacBook-Air.local>
2025-03-20 13:45:52 +0530

    cahnges for accordian

--- src/components/accordian/Accordian.js
+++ src/components/accordian/Accordian.js
@@ -2,15 +2,13 @@ import React, { useState } from "react";
 import { FaAngleDown, FaAngleUp } from "react-icons/fa";
 import { IMAGE_BASE_URL } from "../../utils/urls";
 import ItemDetail from "./ItemDetail";
-const Accordian = ({ data }) => {
-  const [isOpen, setIsOpen] = useState(false);
+const Accordian = ({ data, isOpen, index, setIsOpen }) => {
   const { title, itemCards } = data?.card?.card;
-
   return (
     <div className="accordian shadow rounded-md p-4 m-4 pb-2">
       <div
         className="head cursor-pointer flex justify-between items-center pb-1 mb-3"
-        onClick={() => setIsOpen(!isOpen)}
+        onClick={() => setIsOpen(index)}
       >
         {title}( {itemCards?.length}){isOpen ? <FaAngleUp /> : <FaAngleDown />}
       </div>
